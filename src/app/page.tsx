1b69fa43798535bd87ab5f7985f7af895bc85ab5
kianuetron <kiankian702@gmail.com>
2025-04-17 16:33:44 +0200

    feat:author section and animation added to heroslider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Heroslider from "@/components/Heroslider";
+import Authorscard from "@/components/AuthorsCard";
 
 export default function Home() {
-  return (
-    <main className="min-h-[85vh] flex items-start justify-center pt-8 bg-gray-100">
-      <Heroslider />
-    </main>
-  );
+    return (
+        <main className="min-h-[85vh] bg-gray-100 pt-8">
+            <div className="flex flex-col items-center gap-12">
+                <Heroslider />
+                <Authorscard />
+            </div>
+        </main>
+    );
 }
